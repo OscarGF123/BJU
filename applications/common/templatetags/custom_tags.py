56cba44f5d2c79329682951543842ea67703ccd1
OscarYesid <oscar.gualteros@trycore.com.co>
2025-08-22 11:30:32 -0500

    se agrego la aplicacion common para las vistas base y tambien los tags personalizafos

---
+++ applications/common/templatetags/custom_tags.py
@@ -0,0 +1,24 @@
+from django import template
+from django.db.models import Model
+from django.utils import timezone
+
+
+register = template.Library()
+
+@register.simple_tag()
+def obtener_campos(modelo: Model, titulo=False):
+    # Obtener solo los campos del modelo
+    campos_modelo = [i.name for i in modelo._meta.fields if not i.name.endswith('_ptr')]
+
+    # Coloca la primera letra de cada palabra en la lista si asi se requiere
+    campos_modelo = [i.capitalize() for i in campos_modelo] if titulo else campos_modelo
+    return campos_modelo
+
+@register.simple_tag()
+def obtener_atributo(modelo, campo):
+
+    # Formatear las fechas
+    if (campo.__eq__("fecha_actualizacion") or campo.__eq__("fecha_creacion")):
+        tiempo_local = timezone.localtime(getattr(modelo, campo))
+        return tiempo_local.strftime("%d/%m/%Y %H:%M:%S")
+    return getattr(modelo, campo)
