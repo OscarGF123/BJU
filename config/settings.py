bc73df30edb127dd3e73b6371819b58237574daf
OscarYesid <oscar.gualteros@trycore.com.co>
2025-11-20 23:26:03 -0500

    Se configuirarion variables necesarias para el funcionamiento de las imagenes estaticas

--- config/settings.py
+++ config/settings.py
@@ -179,6 +179,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configuración adicional de whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# WHITENOISE_ROOT = MEDIA_ROOT
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
