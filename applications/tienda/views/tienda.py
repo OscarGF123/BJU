819d6e50970d8a9c29d197d73ed668130f522947
OscarYesid <oscar.gualteros@trycore.com.co>
2026-05-17 15:31:42 -0500

    Se eliminaron comentarios de este archivo

--- applications/tienda/views/tienda.py
+++ applications/tienda/views/tienda.py
@@ -4,19 +4,6 @@ from django.db.models import Prefetch
 from applications.productos.models import Producto, Imagen
 # Create your views here.
 
-"""
-Plan para la compra de un producto
-1. el usuario da click en el producto desde el dashboard
-2. redireccionar a una url donde se den mas detalles de producto, la url
-   debe tener como parametro el id del producto
-3. en la pagina redireccionada debe mostrarse la siguiente información
-    3.1. Imagen del producto
-    3.2. Precio del producto
-    3.3. Colores disponibles del producto
-    3.4. Nombre del producto
-    3.5.  
-"""
-
 class PaginaPrincipal(ListView):
     model = Producto
     template_name = 'pagina_principal/tienda.html'
